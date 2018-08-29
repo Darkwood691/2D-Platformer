ee8cbfe8638f6114a7c90f1dec776f95fdf0ad26
Darkwood691 <daleofdarlo@gmail.com>
2018-08-29 22:40:49 +0100

    imporved program scaffoldinf

--- GameLauncher.py
+++ GameLauncher.py
@@ -1,7 +1,21 @@
-from tkinter import *
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
 
+    def create_widgets(self):
+        self.hi_there = tk.Button(self, text="Hello World\n(click me)", command = self.say_hi)
+        self.hi_there.pack(side="top")
 
-root = Tk()
+        self.quit = tk.Button(self, text="QUIT", fg="red",bg="yellow", command=root.destroy)
+        self.quit.pack(side="bottom")
+        #self.quit["bg"]="black"
+    def say_hi(self):
+        print("hi there, everyone!")
 
-root.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
