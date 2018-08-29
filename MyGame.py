1b78cdb76935cbbbbb0278181593395c701757a2
Darkwood691 <daleofdarlo@gmail.com>
2018-08-29 21:18:23 +0100

    Set up the basic pygame framework

--- MyGame.py
+++ MyGame.py
@@ -1,7 +1,30 @@
-print("Hello world")
-name = str(input("What is your name? "))
-if name.lower() == "dale":
-    print("Hello Master")
-else:
-    print("Hello",name)
-    
+#initialisation ---------------------------------------------------
+import pygame
+import time
+
+#pygame.font.init()
+pygame.init()
+
+#colour definitions -----------------------------------------------
+white = (240,240,240)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+grey = (80,80,80)
+
+#variables --------------------------------------------------------
+resolution = [1280,720]
+
+gameDisplay = pygame.display.set_mode(resolution) 
+pygame.display.set_caption("Game") 
+     
+#gameDisplay.fill(0,0,0)
+pygame.display.update()
+time.sleep(3)
+
+def end():
+    pygame.quit()
+    quit()
+
+end()
