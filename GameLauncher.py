2928987c2e4a462cd2ca92e865e2994f1f1bd5d5
Darkwood691 <daleofdarlo@gmail.com>
2018-08-30 16:30:01 +0100

    Added a checkbox with a working variable

--- GameLauncher.py
+++ GameLauncher.py
@@ -1,18 +1,27 @@
 from tkinter import *
 
+
+
 class Application(Frame):
     def __init__(self, master):
         super(Application,self).__init__(master)
         self.grid()
         self.create_widgets()
-
+    
     def create_widgets(self):
-        Button(self, text = "Hello", command = self.hello).grid(row=0,column = 1, sticky="nesw")
-        Label(self,width=5).grid(row=0,column=0,columnspan=1,sticky="nesw")
+        b1=Button(self, text = "Hello", command = self.hello).grid(row=0,column = 1, sticky="nesw")
+        l1=Label(self,width=5).grid(row=0,column=0,columnspan=1,sticky="nesw")
+        cb1=Checkbutton(self,text="Fullscreen",variable=fullscreen,onvalue=1,offvalue=0).grid(row=1,column=1)
+        b2=Button(self,text="Done",command=self.done).grid(row=2,column=1,sticky="nesw")
+        
     def hello(self):
         print("Hello")
+    
+    def done(self):
+        print(fullscreen.get())
 
 root=Tk()
+fullscreen=IntVar()
 root.title("Game Launcher")
 root.minsize(600, 400)
 app=Application(root)
